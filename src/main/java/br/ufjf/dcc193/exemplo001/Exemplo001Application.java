13d00dec4ffec493955580d2e99cb01a567cffbb
Aloizio Júnior <aloizio@ice.ufjf.br>
2021-06-07 20:35:08 -0300

    Adiciona Projeto Spring Bot

---
+++ src/main/java/br/ufjf/dcc193/exemplo001/Exemplo001Application.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.exemplo001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemplo001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exemplo001Application.class, args);
+		System.out.println("Olá");
+	}
+
+}
