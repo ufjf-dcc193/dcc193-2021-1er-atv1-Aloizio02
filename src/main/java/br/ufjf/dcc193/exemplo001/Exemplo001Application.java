949c6f34fd8bde3ec970e6a6134531b87aa2e0a2
Aloizio Junior <aloizio@ice.ufjf.br>
2021-07-04 20:12:08 -0300

    altera controller

--- src/main/java/br/ufjf/dcc193/exemplo001/Exemplo001Application.java
+++ src/main/java/br/ufjf/dcc193/exemplo001/Exemplo001Application.java
@@ -1,14 +1,13 @@
 package br.ufjf.dcc193.exemplo001;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Exemplo001Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Exemplo001Application.class, args);
-		System.out.println("Olá Mundinhoooo");
 	}
-
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@page pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>Olá</title>
+</head>
+<body>
+    <h1> olá mundinho </h1>
+</body>
+</html>
